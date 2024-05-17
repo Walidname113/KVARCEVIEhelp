cb51b62c1fc492190c5b595c67c588f7545036e8
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2024-05-17 05:25:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Расшифровка файла
 openssl aes-256-cbc -d -salt -pbkdf2 -in codecrypt.py -out code.py -k "Ocrestrinated"
 if [ $? -eq 0 ]; then
   echo -e "$GREENЮзербот успешно установлен. Запуск юзербота...$NC"
