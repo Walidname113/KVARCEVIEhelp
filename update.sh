fa566c5900ae83737366f3e14c78c98d06739a02
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2024-05-18 18:34:41 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,21 +6,34 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
 echo -e "$YELLOWПодождите пожалуйста, выполняется обновление...$NC"
+
 cd || { echo -e "$REDНе удалось перейти в домашний каталог!$NC"; exit 1; }
 cd KRAVIENCEhelp || { echo -e "$REDНе удалось перейти в каталог KRAVIENCEhelp!$NC"; exit 1; }
-rm -rf install.sh update.sh code.py amain.py bootlibralies.sh configure.sh words.json > /dev/null 2>&1 || { echo -e "$REDОшибка удаления файлов!$NC"; }
-mv command_prefix.txt chat_ids.txt interval.txt sessioncash.txt send_status.txt ocrestrinatedub.session ~/ > /dev/null 2>&1 || { echo -e "$REDОшибка перемещения файлов!$NC"; }
+rm -rf install.sh update.sh code.py amain.py bootlibralies.sh configure.sh words.json > /dev/null 2>&1
+
+for file in command_prefix.txt chat_ids.txt interval.txt sessioncash.txt send_status.txt ocrestrinatedub.session; do
+    if [ -f "$file" ]; then
+        mv "$file" ~ > /dev/null 2>&1 || { echo -e "$REDОшибка перемещения $file!$NC"; }
+    fi
+done
+
 cd || { echo -e "$REDНе удалось перейти в домашний каталог!$NC"; exit 1; }
-rm -rf KRAVIENCEhelp > /dev/null 2>&1 || { echo -e "$REDОшибка удаления каталога KRAVIENCEhelp!$NC"; }
+rm -rf KRAVIENCEhelp > /dev/null 2>&1
 pkg install git -y > /dev/null 2>&1 || { echo -e "$REDОшибка установки git!$NC"; exit 1; }
 git clone https://github.com/Walidname113/KRAVIENCEhelp/ > /dev/null 2>&1 || { echo -e "$REDОшибка клонирования репозитория!$NC"; exit 1; }
 chmod -R 755 ~/KRAVIENCEhelp || { echo -e "$REDОшибка изменения прав доступа!$NC"; exit 1; }
 cd || { echo -e "$REDНе удалось перейти в домашний каталог!$NC"; exit 1; }
-mv command_prefix.txt chat_ids.txt interval.txt sessioncash.txt send_status.txt ocrestrinatedub.session KRAVIENCEhelp > /dev/null 2>&1 || { echo -e "$REDОшибка перемещения файлов обратно!$NC"; }
+
+for file in command_prefix.txt chat_ids.txt interval.txt sessioncash.txt send_status.txt ocrestrinatedub.session; do
+    if [ -f ~/"$file" ]; then
+        mv ~/"$file" KRAVIENCEhelp > /dev/null 2>&1 || { echo -e "$REDОшибка перемещения $file обратно!$NC"; }
+    fi
+done
+
 cd KRAVIENCEhelp || { echo -e "$REDНе удалось перейти в каталог KRAVIENCEhelp!$NC"; exit 1; }
 
 if [ $? -eq 0 ]; then
-    echo -e "$YELLOWОбновление прошло успешно, но для полного применения обновлений, введите следующую команду: ` cd && cd KRAVIENCEhelp && bash install.sh ` эта команда перезагрузит необходимые данные. Извините, пока что это не поддерживается автоматически!$NC"
+    echo -e "$YELLOWОбновление прошло успешно, но для полного применения обновлений, введите следующую команду: \` cd && cd KRAVIENCEhelp && bash install.sh \` эта команда перезагрузит необходимые данные. Извините, пока что это не поддерживается автоматически!$NC"
 else
     echo -e "$REDОшибка при обновлении!$NC"
 fi
