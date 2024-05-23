d8e3d29536fb6fd962e14bf06a86c875125c74fd
Walidname113 <100084394+Walidname113@users.noreply.github.com>
2024-05-23 20:29:50 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,9 +6,9 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 
 echo -e "$YELLOWПодождите пожалуйста, выполняется обновление...$NC"
-pkg install git
+pkg install git > /dev/null 2>&1
 cd KRAVIENCEhelp
-git pull
+git pull > /dev/null 2>&1
 clear
 echo -e "$GREENОбновление прошло успешно.$NC"
 bash configure.sh
