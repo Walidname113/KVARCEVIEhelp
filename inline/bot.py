0ff66c96ec183c7b2fe93b307ae6743b3836532a
Ocrestrinated <100084394+Walidname113@users.noreply.github.com>
2024-06-13 15:16:43 +0300

    Update bot.py

--- inline/bot.py
+++ inline/bot.py
@@ -1,15 +1,20 @@
 import telebot
 import os
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 token_file_path = os.path.join(script_dir, '../bot_token.txt')
 
 with open(token_file_path, 'r') as file:
-    bot_token = file.read().strip()
+    bot_token = file.readline().strip()
 
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет. Я — твой будущий инлайн бот, а так же помощник. На данный момент я нахожусь в разработке, поэтому, пожалуйста, ожидай моего появления!")
+    bot.reply_to(message, "Привет. Я — твой будущий инлайн бот, а также помощник. На данный момент я нахожусь в разработке, поэтому, пожалуйста, ожидай моего появления!")
 
-bot.polling()
+if __name__ == "__main__":
+    try:
+        bot.polling()
+    except Exception as e:
+        print(f"Ошибка при запуске бота: {e}")
