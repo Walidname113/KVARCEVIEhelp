7276f4aac38e4f5ae92a3a7e061eb8de2c9a8d44
Ocrestrinated <100084394+Walidname113@users.noreply.github.com>
2024-06-13 14:54:46 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,12 +10,21 @@ cd
 pkg install git > /dev/null 2>&1
 cd KVARCEVIEhelp
 
+random_suffix=$(date +%s | sha256sum | base64 | head -c 8)
+old_filename="Codecrypt_old_${random_suffix}.py"
+
 if [ -f Codecrypt.py ]; then
-    rm Codecrypt.py
+    mv Codecrypt.py $old_filename
 fi
 
 git fetch origin > /dev/null 2>&1
 git reset --hard origin/main > /dev/null 2>&1
 clear
 echo -e "$GREENОбновление прошло успешно.$NC"
+
+# Удаляем старый файл с случайным именем
+if [ -f $old_filename ]; then
+    rm $old_filename
+fi
+
 bash install.sh
